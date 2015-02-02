5c4a950bf141db663dd0858d032a9c8f45346b6a
Romans Malinovskis <romans@agiletoolkit.org>
2015-02-02 01:01:22 +0000

    Fix

--- config_inc.php
+++ config_inc.php
@@ -10,21 +10,3 @@ $g_database_name        = $matches[9];
 
 $g_db_type              = 'mysql';
 
-
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', ''     );
-
-define('FS_METHOD', 'direct');
-
-#define('WP_HOME','http://usblog.triumph.com');
-#define('WP_SITEURL','http://usblog.triumph.com');
-
-
-$table_prefix  = 'wp_';
-
-define('WP_DEBUG', false);
-
-if ( !defined('ABSPATH') )
-  define('ABSPATH', dirname(__FILE__) . '/');
-
-require_once(ABSPATH . 'wp-settings.php');
